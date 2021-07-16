aa7f4c63a36fdb9e251430c7da6c26bb0267f010
Nikita Levashov <n.levashev@idaproject.com>
2021-07-16 17:59:47 +0300

    more solutions

---
+++ 771. Jewels and Stones/tests.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+
+from solution import Solution
+
+
+class SolutionTestCase(TestCase):
+    def test_solution(self) -> None:
+        obj = Solution()
+        jewels1 = "aA"
+        stones1 = "aAAbbbb"
+        jewels2 = "z"
+        stones2 = "ZZ"
+        self.assertEqual(obj.numJewelsInStones(jewels1, stones1), 3)
+        self.assertEqual(obj.numJewelsInStones(jewels2, stones2), 0)
